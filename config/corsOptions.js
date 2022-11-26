cbb2b276e798001ec5f2f365489ce7349183a746
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2022-11-26 19:27:07 +0100

    dodan frontend cors options

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
+  "https://decor-aplikacija.onrender.com",
   "http://localhost:3000",
 ];
 
